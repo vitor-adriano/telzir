d4ede91649a84b2c6a6af84b66ece1540397b988
Vitor Adriano Savedra <v4savedra.dev@outlook.com>
2020-07-21 02:30:33 -0300

    chore(server): authentication base routes/controller

--- server/src/app/routes.js
+++ server/src/app/routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 
+const authController = require('../controllers/auth-controller')
+
 const route = express()
 
-route.get('/', (req, res) => res.sendStatus(200))
+route.post('/register', authController.register)
+route.post('/login', authController.login)
 
 module.exports = route
---
+++ server/src/controllers/auth-controller.js
@@ -0,0 +1,39 @@
+const db = require('../app/database')
+
+module.exports = {
+  login: async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await db('users').where({ email, password }).first()
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Conta não encontrada.',
+      })
+    }
+
+    res.json({
+      data: {
+        name: user.name,
+        email: user.email,
+        current_plan: user.current_plan,
+      },
+    })
+  },
+
+  register: async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await db('users').where({ email }).first()
+
+    if (userExists) {
+      return res.status(400).json({
+        message: 'Este e-mail já está cadastrado.',
+      })
+    }
+
+    await db('users').insert({ name, email, password })
+
+    return res.sendStatus(201)
+  },
+}
