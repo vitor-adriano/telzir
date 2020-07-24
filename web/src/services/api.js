eb693ad6e434de74e9c158123479521f61b61cd5
Vitor Adriano Savedra <v4savedra.dev@outlook.com>
2020-07-24 12:34:21 -0300

    refactor(web): set authorization header

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
 })
 
+api.interceptors.request.use(req => {
+  const token = localStorage.getItem('_token')
+
+  if (!!token) {
+    req.headers['Authorization'] = `Bearer ${JSON.parse(token)}`
+  }
+
+  return req
+})
+
 export default api
