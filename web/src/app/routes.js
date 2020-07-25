a2c697f5081585e436bf67e49bec3e99310be2f8
Vitor Adriano Savedra <v4savedra.dev@outlook.com>
2020-07-25 16:25:58 -0300

    chore(web): restrict and private routes

--- web/src/app/routes.js
+++ web/src/app/routes.js
@@ -1,18 +1,42 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route as BaseRoute, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import Login from 'pages/login'
 import Register from 'pages/register'
 import Dashboard from 'pages/dashboard'
 
+const Route = ({ component: Component, isPrivate, ...baseProps }) => {
+  const { isAuthenticated } = useSelector(state => state.user)
+
+  return (
+    <BaseRoute
+      {...baseProps}
+      render={props => {
+        return isPrivate ? (
+          isAuthenticated ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to="/login" />
+          )
+        ) : !isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/dashboard" />
+        )
+      }}
+    />
+  )
+}
+
 const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/" render={() => <Redirect to="/login" />} />
+      <BaseRoute exact path="/" render={() => <Redirect to="/login" />} />
 
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route isPrivate path="/dashboard" component={Dashboard} />
     </Switch>
   )
 }
